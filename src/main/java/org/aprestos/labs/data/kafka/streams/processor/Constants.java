9c5c0416688a9080f6d685a994d4c04e690a06fa
jtviegas <jtviegas.js@google.com>
2017-08-11 00:19:47 +0100

    ...

--- src/main/java/org/aprestos/labs/data/kafka/streams/processor/Constants.java
+++
@@ -1,9 +0,0 @@
-package org.aprestos.labs.data.kafka.streams.processor;
-
-public interface Constants {
-	static String APP_NAME = "kafka-streams-processor"
-			, ENV_KAFKA_CONFIG = "KAFKA_CONFIG"
-			, ENV_ZOOKEEPER_CONFIG = "ZOOKEEPER_CONFIG"
-			;
-	
-}
